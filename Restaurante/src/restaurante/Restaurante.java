8f1b8402a2126354142a24e50c92998a141396d8
chebatian <chebatian@gmail.com>
2017-12-07 21:32:23 -0300

    Comentario de prueba

--- Restaurante/src/restaurante/Restaurante.java
+++ Restaurante/src/restaurante/Restaurante.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -31,7 +31,7 @@ public class Restaurante {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
-//SEBA:        
+//SEBA: Test...        
 //new LoginDialog(null, false).setVisible(true);
 
         preCargaDeDatos();
